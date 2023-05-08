22d44d0c9aba8b080689ff8c93bf547143dd9797
magicalbeauty91 <magicalbeauty91@gmail.com>
2023-05-08 01:23:17 -0500

    fixed hero section and made it mobile responsive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,11 +2,21 @@ import "./Hero.css";
 import React from 'react'
 import IntroImg from "../assests/1767974.webp";
 
+import { Link } from "react-router-dom";
+
 const Hero = () => {
   return (
     <div className="hero">
       <div className="mask">
         <img className="intro-img" src={IntroImg} alt="Intro image" />
+      </div>
+      <div className="content">
+        <p>Hi, I am Marissa</p>
+        <h1>MERN Full Stack Developer</h1>
+        <div>
+          <Link to="/project" className="btn">Projects</Link>
+          <Link to="/contact" className="btn btn-light">Contact</Link>
+        </div>
       </div>
     </div>
   )
