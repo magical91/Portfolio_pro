745f2348d0602da1ae2e6abeed65550c6e30ec1d
magicalbeauty91 <magicalbeauty91@gmail.com>
2023-05-09 00:40:04 -0500

    finished contact section and added a contact form

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from "../components/Hero"
 import Footer from "../components/Footer"
+import Work from "../components/Work"
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Work />
       <Footer />
     </div>
   )
